041cc96c162e37b74224a622292488107880c395
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-10-29 18:55:08 +0300

    init project

---
+++ config/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+from config import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Board API",
+        default_version='v1',
+        description="Board description",
+        terms_of_service="http://89.169.167.48",
+        contact=openapi.Contact(email="AlexeyDenisenko2703@yandex.ru"),
+        license=openapi.License(name="Alex License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('swagger<format>/',
+         schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+    path('admin/', admin.site.urls),
+]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
